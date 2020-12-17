478aa7c7051f0251f2fde5a760ec80e99d8546ef
rivalda.bedini@fshnstudent.info <rivalda.bedini@fshnstudent.info>
2020-12-17 19:42:09 +0100

    Ushtrimi2

---
+++ Laborator_A2_3/Laborator_A2_3/Rivalda/WebForm2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laborator_A2_3.Rivalda
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(DropDownList1.SelectedItem != null)
+            {
+                if (!string.IsNullOrEmpty(DropDownList1.SelectedItem.Text))
+                {
+                    if(DropDownList1.SelectedItem.Text.Contains("E kuqe"))
+                    {
+                        Label2.Text = "Numri i elementeve te listes eshte " + DropDownList1.Items.Count;
+                    }
+                    else
+                    {
+                        TextBox txt = new TextBox();
+                        txt.Text = "Ngjyra e zgjedhur eshte " + DropDownList1.SelectedItem.Text;
+                        form1.Controls.Add(txt);
+
+                    }
+
+                }
+            }
+        }
+    }
+}
