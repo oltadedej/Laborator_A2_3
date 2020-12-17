4a768537e9df30d7ec68e7ab121bcad2d8ff6282
Olta DEDEJ_EXT <olta.dedej_ext@alten.it>
2020-12-17 18:24:29 +0100

    .....

---
+++ Laborator_A2_3/Laborator_A2_3/Laborator2/Ushtrim2.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laborator_A2_3.Laborator2
+{
+    public partial class Ushtrim2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            label1.Text = "Olta Dedej";
+            femer.Checked = true;
+
+            AfishimMesazhi.Text = "Pershendetje:" + label1.Text + " " + "Gjinia juaj eshte: " + femer.Text + "Data e sotm eshte: " + DateTime.Now.ToString("dd/MM/yyyy");
+          //  AfishimMesazhi.Text = $"Pershendetje: {label1.Text} Gjinia juaj eshte {femer.Text} data e sotme eshte {DateTime.Now.ToString("dd/MM/yyyy")}";
+
+        }
+    }
+}
---
+++ Laborator_A2_3/Laborator_A2_3/Laborator2/Ushtrimi1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laborator_A2_3.Laborator2
+{
+    public partial class Ushtrimi1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
