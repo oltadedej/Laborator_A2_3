8b293f436266b02bedff3139e605b3cc3c7bba5a
Olta DEDEJ_EXT <olta.dedej_ext@alten.it>
2020-12-17 19:45:42 +0100

    ..

---
+++ Laborator_A2_3/Laborator_A2_3/Ushtrimi8.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ushtrimi8.aspx.cs" Inherits="Laborator_A2_3.Ushtrimi8" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="true"  OnTextChanged="AfishoniText"></asp:TextBox>
+
+            <asp:Label runat="server" ID="lbl1" ></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Laborator_A2_3/Laborator_A2_3/Ushtrimi8.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laborator_A2_3
+{
+    public partial class Ushtrimi8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        protected void AfishoniText(object sender, EventArgs e)
+        {
+
+            lbl1.Text = "";
+            if (!String.IsNullOrEmpty(TextBox1.Text))
+            {
+                if(TextBox1.Text.Contains(" "))
+                {
+                    string[] array = TextBox1.Text.Split(' ');
+
+                    lbl1.Text = "Numri i elementeve te array eshte:" + array.Length;
+                   
+                }
+
+            }
+
+        }
+    }
+}
