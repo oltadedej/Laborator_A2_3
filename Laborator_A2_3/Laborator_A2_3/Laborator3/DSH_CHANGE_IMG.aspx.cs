21ff8ff8d6a6efecfb8956874d7c5be80e2f0774
Olta DEDEJ_EXT <olta.dedej_ext@alten.it>
2021-01-21 10:39:06 +0100

    laborator3

---
+++ Laborator_A2_3/Laborator_A2_3/Laborator3/DSH_CHANGE_IMG.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laborator_A2_3.Laborator3
+{
+    public partial class DSH_CHANGE_IMG : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Image1.ImageUrl = "~/img/download_new.png";
+        }
+    }
+}
